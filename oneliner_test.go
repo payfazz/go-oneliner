d3218d89c354de4ea0cc6748524058ef0af31128
Kurnia D Win <kurnia.d.win@gmail.com>
2019-07-14 16:36:43 +0700

    adding benchmark

--- oneliner_test.go
+++ oneliner_test.go
@@ -2,6 +2,7 @@ package oneliner
 
 import (
 	"bytes"
+	"io/ioutil"
 	"testing"
 )
 
@@ -11,5 +12,13 @@ func Test1(t *testing.T) {
 	ol.Write([]byte("a\nb"))
 	if !bytes.Equal(b.Bytes(), []byte("\"a\\nb\"\n")) {
 		t.FailNow()
+	}
+}
+
+func Benchmark1(b *testing.B) {
+	ol := Wrap(ioutil.Discard)
+	data := []byte("test")
+	for i := 0; i < b.N; i++ {
+		ol.Write(data)
 	}
 }
